621de041cf3bc2a169afc5b332474f49c639002c
V6429 <cb.en.p2ebs21015@cb.students.amrita.edu>
2022-06-09 16:05:35 +0530

    last updates before implementing TX RX b/w Rpi & node2

--- node_2_stm32/stm_cubeide_ws/Core/lora/src/LoRa.c
+++ node_2_stm32/stm_cubeide_ws/Core/lora/src/LoRa.c
@@ -85,7 +85,6 @@ void LoRa_gotoMode(LoRa* _LoRa, int mode){
 	//HAL_Delay(10);
 }
 
-
 /* ----------------------------------------------------------------------------- *\
 		name        : LoRa_readReg
 
@@ -471,7 +470,7 @@ uint16_t LoRa_init(LoRa* _LoRa){
 	uint8_t    data;
 	uint8_t    read;
 
-	if(LoRa_isvalid(_LoRa)){
+
 		// goto sleep mode:
 			LoRa_gotoMode(_LoRa, SLEEP_MODE);
 			HAL_Delay(10);
@@ -528,8 +527,6 @@ uint16_t LoRa_init(LoRa* _LoRa){
 				return LORA_OK;
 			else
 				return LORA_NOT_FOUND;
-	}
-	else {
-		return LORA_UNAVAILABLE;
-	}
+
+
 }
